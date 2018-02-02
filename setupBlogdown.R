329b250e4500184351e6462ad0236019c8ee138c
Adam <12676475+adam-sampson@users.noreply.github.com>
2018-02-01 22:05:12 -0500

    Graphs

--- setupBlogdown.R
+++ setupBlogdown.R
@@ -1,7 +1,10 @@
 require(blogdown)
-blogdown::install_hugo()
+#blogdown::install_hugo()
 
-blogdown::new_site(dir="./blogdown", sample = FALSE, theme = "gcushen/hugo-academic", theme_example = FALSE)
+#blogdown::new_site(dir="./blogdown", sample = FALSE, theme = "gcushen/hugo-academic", theme_example = FALSE)
 
 setwd("~/MSA/sampsonblogdown/blogdown")
 blogdown::serve_site()
+blogdown::build_site()
+#blogdown::new_content(path = "/home/about.md")
+#blogdown::new_content(path = "/home/hero.md")
